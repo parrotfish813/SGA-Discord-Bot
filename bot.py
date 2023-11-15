0ff710f49eed8bd7abfc2b989bde9f10b80ceeb6
Brandon Townes <bmt813@gmail.com>
2023-11-14 22:14:18 -0500

    Created enviroment

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import discord
-import responces
+from discord.ext import commands
 import openpyxl
 import os
 
 def run_discord_bot():
-    bot = discord.Client()
+    intents = discord.Intents.all()  
+
+    bot = commands.Bot(command_prefix='!', intents=intents)
+
+    # bot = discord.Client()
 
     @bot.event
     async def on_ready():
         print(f'{bot.user} is now running!')
-
-    bot.run(os.getenv("TOKEN"))
 
     @bot.command(name='positions')
     async def positions(ctx):
@@ -19,7 +21,8 @@ def run_discord_bot():
 
         for row in sheet.iter_rows(min_row=2, values_only=True):
             formatted_text = f"Name: {row[0]}, Position: {row[1]}, Year: {row[2]}"  
-            channel_id = 1174085794231767050  
-            channel = bot.get_channel(channel_id)
+            channel = bot.get_channel(os.getenv("IMPORT_INFORMATION_CHANNEL_ID"))
             await channel.send(formatted_text)
+
+    bot.run(os.getenv("TOKEN"))
 
