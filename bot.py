9470f8c6bdfe3a637eb8d6617e89e6fc11d372f4
Brandon Townes <bmt813@gmail.com>
2023-11-15 17:20:23 -0500

    Added indavidual functions for each branch

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
+from discord import Embed
 import openpyxl
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def run_discord_bot():
 
@@ -12,28 +10,83 @@ def run_discord_bot():
 
     bot = commands.Bot(command_prefix='!', intents=intents)
 
+    token = 'MTE3NDE0NjgxMjgxNTM2NDEwNg.GW696D.hOcf39cYqEHxxx5p8s4gpEnEHiSXkUe4zDneKY'
+    important_information_channel = 1174466121659859055
+
     @bot.event
     async def on_ready():
         print(f'{bot.user} is now running!')
 
-    @bot.command(name='positions')
-    async def positions(ctx):
+    @bot.command(name='positions_exec_board')
+    async def positions_exec_board(ctx):
+        workbook = openpyxl.load_workbook('member_list.xlsx')
+        sheet = workbook.active
+        
+        channel = bot.get_channel(important_information_channel)
+        
+        branch = f"**EXECUTIVE BOARD**\n-----------------------------------------------------------"
+        target_sheet_name = "executive_board"
+
+        if target_sheet_name in workbook.sheetnames:
+            sheet = workbook[target_sheet_name]
+            await channel.send(branch)
+
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                formatted_text = f"**{row[0]}:** {row[1]}"
+                await channel.send(formatted_text)
+
+    @bot.command(name='positions_exec_cabinet')
+    async def positions_exec_cabinet(ctx):
+        workbook = openpyxl.load_workbook('member_list.xlsx')
+        sheet = workbook.active
+        
+        channel = bot.get_channel(important_information_channel)
+        
+        branch = f"**EXECUTIVE CABINET**\n-----------------------------------------------------------"
+        target_sheet_name = "executive_cabinet"
+
+        if target_sheet_name in workbook.sheetnames:
+            sheet = workbook[target_sheet_name]
+            await channel.send(branch)
+
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                formatted_text = f"**{row[0]}:** {row[1]}"
+                await channel.send(formatted_text)
+
+    @bot.command(name='positions_legislative')
+    async def positions_legislative(ctx):
         workbook = openpyxl.load_workbook('member_list.xlsx')
         sheet = workbook.active
 
-        for row in sheet.iter_rows(min_row=2, values_only=True):
-            formatted_text = f"Name: {row[0]}, Position: {row[1]}, Year: {row[2]}"  
-            channel = bot.get_channel(os.getenv("IMPORT_INFORMATION_CHANNEL_ID"))
-            await channel.send(formatted_text)
+        channel = bot.get_channel(important_information_channel)
 
-    token = os.getenv("TOKEN")
-    print(f'Token: {token}')
+        branch = f"**LEGISLATIVE**\n-----------------------------------------------------------"
+        target_sheet_name = "legislative"
 
-    id = os.getenv("IMPORT_INFORMATION_CHANNEL_ID")
-    print(f'ID: {id}')
+        if target_sheet_name in workbook.sheetnames:
+            sheet = workbook[target_sheet_name]
+            await channel.send(branch)
 
-    if token is None:
-        print("Error: TOKEN environment variable is not set.")
-        return
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                formatted_text = f"**{row[0]}:** {row[1]}"
+                await channel.send(formatted_text)
+
+    @bot.command(name='positions_judicial')
+    async def positions_judicial(ctx):
+        workbook = openpyxl.load_workbook('member_list.xlsx')
+        sheet = workbook.active
+        
+        channel = bot.get_channel(important_information_channel)
+        
+        branch = f"**JUDICIAL**\n-----------------------------------------------------------"
+        target_sheet_name = "judicial"
+
+        if target_sheet_name in workbook.sheetnames:
+            sheet = workbook[target_sheet_name]
+            await channel.send(branch)
+
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                formatted_text = f"**{row[0]}:** {row[1]}"
+                await channel.send(formatted_text)
 
     bot.run(token)
