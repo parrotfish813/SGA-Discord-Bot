0c85309a825a1f298cf5988a46095ce4602681a7
Brandon Townes <bmt813@gmail.com>
2023-11-19 14:20:18 -0500

    Fixed the envorimental variables

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 from discord import Embed
 import openpyxl
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 def run_discord_bot():
 
@@ -10,16 +13,25 @@ def run_discord_bot():
 
     bot = commands.Bot(command_prefix='!', intents=intents)
 
-    token = 'MTE3NDE0NjgxMjgxNTM2NDEwNg.GW696D.hOcf39cYqEHxxx5p8s4gpEnEHiSXkUe4zDneKY'
-    important_information_channel = 1175062419815538740
-    exec_information_channel = 1151911705610289203
-    legislative_information_channel = 1151912030828245062
-    judicial_information_channel = 1151912235560615999
+    token: str = os.getenv("DISCORD_TOKEN")
+    # Convert the channel IDs to integers
+    important_information_channel = int(os.getenv("IMPORTANT_INFORMATION_CHANNEL"))
+    exec_information_channel = int(os.getenv("EXEC_INFORMATION_CHANNEL"))
+    legislative_information_channel = int(os.getenv("LEGISLATIVE_INFORMATION_CHANNEL"))  
+    judicial_information_channel = int(os.getenv("JUDICIAL_INFORMATION_CHANNEL"))
+    lec_information_channel  = int(os.getenv("LEC_INFORMATION_CHANNEL"))
+    abc_information_channel = int(os.getenv("ABC_INFORMATION_CHANNEL"))
+    acc_information_channel = int(os.getenv("ACC_INFORMATION_CHANNEL"))
+    pec_information_channel = int(os.getenv("PEC_INFORMATION_CHANNEL"))
+    media_information_channel = int(os.getenv("MEDIA_INFORMATION_CHANNEL"))
 
+    print(f"important_information_channel: {important_information_channel}")
 
     @bot.event
     async def on_ready():
         print(f'{bot.user} is now running!')
+
+    """ POSITIONS COMMANDS """
 
     @bot.command(name='positions_exec_board')
     async def positions_exec_board(ctx):
@@ -93,6 +105,9 @@ def run_discord_bot():
                 formatted_text = f"**{row[0]}:** {row[1]}"
                 await channel.send(formatted_text)
 
+    
+    """ INFORMATIONAL COMMANDS"""
+
     @bot.command(name='branch_info_exec')
     async def branch_info_exec(ctx):
         channel = bot.get_channel(exec_information_channel)
@@ -112,6 +127,41 @@ def run_discord_bot():
         channel = bot.get_channel(judicial_information_channel)
 
         formatted_text = f"The Student Government Association's (SGA) Judicial Branch is comprised of the SGA Supreme Court and the SGA Office of Student Counsel.\n\nThe Florida Polytechnic University SGA Supreme Court represents the judicial authority of the Student Government Association, including in matters relating to the interpretation of the Constitution and related statutes. There are seven Justices on the Supreme Court, including the Chief Justice, Raking Justice, Senior Justice, and four Associate Justices.\n\nThe Florida Polytechnic University SGA Office of Student Counsel provides the student body access to professional counselors during court and administrative proceedings. The Office of Student Counsel provides assistance to students in matters pertaining to alleged Student Code of Conduct violations.\n\nJudicial Branch email: sga-judicial@floridapoly.edu"
+        await channel.send(formatted_text)
+
+    @bot.command(name='branch_info_lec')
+    async def branch_info_lec(ctx):
+        channel = bot.get_channel(lec_information_channel)
+
+        formatted_text = f"The Florida Polytechnic University SGA Legislative Executive Committee (LEC) is comprised of the SGA Senate President, SGA Senate Pro Tempore, and all Committee Chairs.\n\nLEC is where all senate chairs discuss important updates from their respective committees, progress on individual committee projects, as well as any issues that may have come up during their respective meetings that needs to be addressed.\n\nPotential issues that students are facing are brought to this committee, and LEC determines what direction the individual committees can take to address these concerns, either by finding resources or by determining possible solutions."
+        await channel.send(formatted_text)
+
+    @bot.command(name='branch_info_abc')
+    async def branch_info_abc(ctx):
+        channel = bot.get_channel(abc_information_channel)
+
+        formatted_text = f""
+        await channel.send(formatted_text)
+
+    @bot.command(name='branch_info_acc')
+    async def branch_info_acc(ctx):
+        channel = bot.get_channel(acc_information_channel)
+
+        formatted_text = f""
+        await channel.send(formatted_text)
+
+    @bot.command(name='branch_info_pec')
+    async def branch_info_pec(ctx):
+        channel = bot.get_channel(pec_information_channel)
+
+        formatted_text = f""
+        await channel.send(formatted_text)
+
+    @bot.command(name='branch_info_media')
+    async def branch_info_media(ctx):
+        channel = bot.get_channel(media_information_channel)
+
+        formatted_text = f"The Florida Polytechnic University SGA Department of Student Media is comprised of the SGA Director of Student Media and additional deputies, such as the Deputy of Communication, the Deputy of Marketing, and the Deputy of Enforcement.\n\nThe Department of Student Media is responsible for ensuring that the student body is aware of SGA and SGA’s outreach to students, along with supporting media groups, on campus.\n\nThis department oversees any and all social media, as well as create flyers and merchandise, for SGA and RSOs."
         await channel.send(formatted_text)
 
     bot.run(token)
